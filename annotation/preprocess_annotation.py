370219ad10bd75a514b67291f0bd49b25100a201
Ibtissam Saadi <56130568+SAADI-Ibtissam@users.noreply.github.com>
2026-06-23 10:53:57 +0200

    Update DFEW_SOURCE path to use relative directory

--- annotation/preprocess_annotation.py
+++ annotation/preprocess_annotation.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from glob import glob
 
-DFEW_SOURCE = "Code4/DFEWdataset"
+DFEW_SOURCE = "./datasets/DFEWdataset"
 DFEW_CLIP_SOURCE = os.path.join(DFEW_SOURCE, "Clip", "small_clip_224x224")
 
 def main():
@@ -13,7 +13,7 @@ def main():
         data = pd.read_csv(anno_file)
         write_data = []
         write_file_path = "DFEW_{}_{}.txt".format(anno_file.split("/")[-1].split(".")[0], anno_file.split("/")[-2])
-        write_file_path = os.path.join("Code4/DFEWdataset/Annotation", write_file_path)
+        write_file_path = os.path.join("./datasets/DFEWdataset/Annotation", write_file_path)
         print("ok2")
 
         # Create the directory if it does not exist
@@ -21,7 +21,7 @@ def main():
 
         for index, row in data.iterrows():
             video_name = str(row["video_name"]).zfill(5)
-            full_video_name = os.path.join("Code4/DFEWdataset/Clip/clip_224x224", video_name)
+            full_video_name = os.path.join("./datasets/DFEWdataset/Clip/clip_224x224", video_name)
             label = str(row["label"] - 1)
             clip_folder_path = os.path.join(DFEW_SOURCE, "Clip", "small_clip_224x224", video_name)
             
